901cb609c3e5c08857459b8a16e695e8562fe84a
jairingonzales10-debug <jairingonzales10@gmail.com>
2026-08-17 17:45:42 -0600

    ORDER BY ASC laboratorios.sql

--- 01_creacion_tablas/laboratorios.sql
+++ 01_creacion_tablas/laboratorios.sql
@@ -1,2 +1,4 @@
 CREATE TABLE laboratorios (id_laboratorio SERIAL PRIMARY KEY, nombre VARCHAR(100), NOT NULL, telefono VARCHAR(15), email VARCHAR(100));
+SELECT * FROM laboratorios
+ORDER BY id_laboratorio ASC;
 
