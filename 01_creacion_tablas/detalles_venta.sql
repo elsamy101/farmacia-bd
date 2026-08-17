cd9f5e532007e542c52ce8ae09686ee59c4faffc
jairingonzales10-debug <jairingonzales10@gmail.com>
2026-08-17 17:41:51 -0600

    Orden by asc Update detalles_venta.sql

--- 01_creacion_tablas/detalles_venta.sql
+++ 01_creacion_tablas/detalles_venta.sql
@@ -1,1 +1,4 @@
 CREATE TABLE detalles_venta (id_detalle SERIAL PRIMARY KEY, id_venta INT NOT NULL, id_lote INT NOT NULL, cantidad INT, precio_unitario DECIMAL(10, 2), subtotal DECIMAL(10, 2), FOREIGN KEY(id_venta) REFERENCES ventas(id_venta), FOREIGN KEY (id_lote) REFERENCES lotes(id_lote));
+SELECT * FROM detalles_venta
+ORDER BY id_venta ASC;
+
