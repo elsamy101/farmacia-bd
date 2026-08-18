4f3970238bb44ca4ba466ce4e295e78a6fdd5e97
jairingonzales10-debug <jairingonzales10@gmail.com>
2026-08-17 18:13:31 -0600

    ORDER BY ASC recetas_medicas.sql

--- 01_creacion_tablas/recetas_medicas.sql
+++ 01_creacion_tablas/recetas_medicas.sql
@@ -1,1 +1,3 @@
 CREATE TABLE recetas_medicas (id_receta SERIAL PRIMARY KEY, numero_receta VARCHAR(50) NOT NULL UNIQUE, fecha_emision DATE NOT NULL, medico VARCHAR(100), paciente VARCHAR(100), indicaciones TEXT);
+SELECT * FROM recetas_medicas
+ORDER BY id_receta ASC;
