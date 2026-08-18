adfbd12f6ef8aa3154a7aba398206a7b7484885b
jairingonzales10-debug <jairingonzales10@gmail.com>
2026-08-17 18:11:55 -0600

    ORDER BY ASC medicamentos.sql

--- 01_creacion_tablas/medicamentos.sql
+++ 01_creacion_tablas/medicamentos.sql
@@ -1,1 +1,3 @@
 CREATE TABLE medicamentos (id_medicamento SERIAL PRIMARY KEY, nombre VARCHAR(100) NOT NULL, descripcion TEXT, precio_unitario DECIMAL(10, 2), id_laboratorio INT NOT NULL, FOREIGN KEY (id_laboratorio) REFERENCES laboratorios(id_laboratorio));
+SELECT * FROM medicamentos
+ORDER BY id_medicamento ASC;
