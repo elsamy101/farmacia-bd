5f1ef63464d4976af0113fdfcac6490513bb9712
jairingonzales10-debug <jairingonzales10@gmail.com>
2026-08-17 18:14:49 -0600

    ORDER BY ASCventas.sql

--- 01_creacion_tablas/ventas.sql
+++ 01_creacion_tablas/ventas.sql
@@ -1,1 +1,3 @@
 CREATE TABLE ventas (id_venta SERIAL PRIMARY KEY, fecha_venta DATE NOT NULL, id_receta INT, total DECIMAL(10, 2), FOREIGN KEY(id_receta) REFERENCES recetas_medicas(id_receta));
+SELECT * FROM ventas
+ORDER BY id_venta, fecha_venta, id_receta ASC;
